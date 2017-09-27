be980e41311400ce1f603dde5dc059e885c19339
Jasdeep <jasdeepchhabra94@gmail.com>
2017-09-27 18:23:50 +0530

    comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
-
-
+//function to make request to API.ai.
 function make_request(text){
 	const client = new ApiAi.ApiAiClient({accessToken: '518d3765d1154ab0b5d0f3f2863945b8'});
 	const promise = client.textRequest(text);
@@ -12,7 +11,7 @@ promise
 
 
 
-
+//response from request
 function handleResponse(serverResponse) {
 		console.log(serverResponse)
         text_response=(serverResponse.result.fulfillment.speech);
@@ -25,46 +24,16 @@ function handleResponse(serverResponse) {
 function handleError(serverError) {
         console.log(serverError);
 }
+
+
+//adjusts scrolling while chat proceeds
 function adjust_scroll(){
 	$('#scrollable-content').animate({scrollTop:
         	$('#scrollable-content').prop('scrollHeight')},1000);
 }
 
 
-/*
-
-function send_text_request(text){
-
-
-	$.ajax({
-
-		type:'POST',
-		url:"https://api.api.ai/v1/query",
-		contentType:"application/json; charset=utf-8",
-		dataType:"json",
-		headers:{
-			"Authorization":"Bearer "+"518d3765d1154ab0b5d0f3f2863945b8"
-		},
-		data:JSON.stringify({query:text,lang:'en',sessionId:"bankbot"}),
-
-	}).done(function(response){
-		
-		console.log(response)
-		handle_response(response.result.speech)
-
-	});
-
-};
-
-
-function handle_response(text_response){
-
-	$("#scrollable-content").append("<p><b>MeBot: </b>"+text_response+"</p>");
-
-
-
-}
-*/
+//combines all above functions to generate a reply
 function reply(){
 		var text=$('#input_space').val();
 		$("#input_space").val("");
@@ -74,22 +43,22 @@ function reply(){
 		make_request(text);
 		
 
-
-
-		
 }
 
-
+//pressing send
 $('#myForm').submit(function(e){
 	e.preventDefault();
 	reply();
 });
+
+//[ressing enter]
 $('#input_space').keypress(function(e){
 	if(e.charCode==13){
 		reply();
 	}
 })
 
+//toggle button
 $('.button').click(function(){
 	$('#dialog-window').toggle();
 })
